f9b3c400175317a8a8e1454fbb51a84bf35c4dd7
Erreniel <shannya.michelia@binus.ac.id>
2020-11-27 21:06:13 +0700

    push main

---
+++ views.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "../controllers/controllers.h"
+
+
+int main {
+   scanf ("%d %d", &t.totalPatient, &t.totalCure);
+   int n=t.totalpatient;
+
+   for (int i=0; i<n; i++){
+       scanf ("%d %[^\n] %d - %[^\n]", &p.day, p.month, &p.year, p.name);
+   }
+   
+   int out=t.totalPatient - t.totalCure
+   printf ("Need %d more cure\n",out); 
+   for (int j=0; j<out; j++){
+
+   }
+
+   return 0;
+}
